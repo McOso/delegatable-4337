a3486ff7a51dca925b1bf3d22c5c2af616427daf
Dan Finlay <542863+danfinlay@users.noreply.github.com>
2023-04-25 11:27:54 -0700

    Add notes on transitive paid delegation

--- test/paid_delegation.test.ts
+++ test/paid_delegation.test.ts
@@ -33,10 +33,12 @@ describe("paid delegation", function () {
      * This test is a placeholder for a test that checks that a user can delegate a right to transfer to a dex.
      * A first caveat would be one that ensures this delegation can only transfer the specified asset.
      * A second caveat could be an expiration date, or even a price changing over time.
+     * A third caveat restricts buying to a known set of buyers.
      * The recipient of the delegation could be a simple DEX.
      * 
-     * The DEX should allow a user to purchase the asset with an ERC20 token.
+     * The DEX should allow an approved user to purchase the asset with an ERC20 token.
      * The DEX should allow the buyer to provide their allowance to the DEX as a delegation also.
+     * function buyAsset (Delegation buyerTokenAllowance, Delegation sellerTokenOffer); 
      * 
      * Together, this means one transaction can allow a user to purchase an asset with an ERC20 token.
      * 
@@ -44,6 +46,11 @@ describe("paid delegation", function () {
      * Another variation of this test could allow the seller to not have their account on-chain yet:
      * In that case, a caveat of the original delegation might be an initCode consuming contract publisher,
      * It may also include a caveat for ensuring the asset is published on chain as part of the transaction.
+     *
+     * Still needs strategy for transitive delegation of this right.
+     * function initiateTrade (Delegation buyerTokenAllowance, Delegation[] tokenSellingRight);
+     * Walks the tokenSellingRight, needs to be able to perform the swaps in sequence.
+     * 
      */
     it("Can delegate a right to transfer to a dex")
 })
